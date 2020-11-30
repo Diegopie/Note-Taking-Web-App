884b4f5adcea08c81a56f66f0f8091e24db6ac58
Diego Hernandez <ishtaracademy@outlook.com>
2020-11-30 12:37:06 -0700

    Updated read me; removed console logs; connected with heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,8 @@
 const fs = require("fs")
 const noteData = require("../db/db.json");
 
-// Testing to see if a can add to noteDAta
-// console.log("fresh");
-// console.log(noteData)
-// noteData.push({title: "push test", text: "push push"})
-// console.log(noteData);
-// const parseData = JSON.parse(noteData);
 let newID = noteData.length
-console.log(newID);
+// console.log(newID);
 
 // console.log(noteData.length);
 
@@ -16,25 +10,25 @@ module.exports = function(app) {
   app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) throw err;
-      console.log(data);
+      // console.log(data);
       res.send(JSON.parse(data));
     });
   });
 
   // Route for Saving Notes
   app.post("/api/notes", (req, res) => {
-        console.log("Post");
-        console.log(req.body);
+        // console.log("Post");
+        // console.log(req.body);
     // Create New Keu ID
     const { id } = req.body
-    // Maybe add an ID based on noteaDat.length, then in app.delete, do another for in that updates all the ids
+    // Maybe add an ID based on noteData.length, then in app.delete, do another for in that updates all the ids
 
     // Set ID value a unique ID
     req.body.id = newID
-        console.log(req.body);
+        // console.log(req.body);
     // Push New Note to notaData array
     noteData.push(req.body)
-        console.log(noteData);
+        // console.log(noteData);
     // Overwrite Old db File with New noteData Array
     fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
       if (err) throw err;
@@ -46,17 +40,17 @@ module.exports = function(app) {
   app.delete("/api/notes/:id", (req, res) => {
     // Get ID to Be Deleted
     const toDelete = req.params.id;
-        console.log("To Delete: ", toDelete);
+        // console.log("To Delete: ", toDelete);
     // Loop Through noteData to Find the Selected ID
     noteData.forEach(del => {
       if (del.id == toDelete) {
-            console.log("Done it: ", del);
+            // console.log("Done it: ", del);
         // Find the Index Value of the Match
         const index = noteData.indexOf(del);
-            console.log("Index of array: ", index);
+            // console.log("Index of array: ", index);
         // Remove it From Array
         noteData.splice(index, 1);
-            console.log("New note data: ", noteData);
+            // console.log("New note data: ", noteData);
       }
     });
     // Update the IDs of All the Notes
@@ -65,7 +59,7 @@ module.exports = function(app) {
       ind.id = i;
       i++;
     })
-    console.log("update ids: ", noteData);
+    // console.log("update ids: ", noteData);
     // Update DB with New Array
     fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
       if (err) throw err;
@@ -74,8 +68,5 @@ module.exports = function(app) {
     res.send();
   })
 }
-
-
-// So it doesnt look like unique ids are working
 
 // Back uo and work on post then delete
